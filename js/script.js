69e7b70eadd4b026708ca04629b4a248291e5d8a
Iky61 <dicky.gps105@gmail.com>
2023-02-05 17:46:47 +0800

    add figure 2

--- js/script.js
+++ js/script.js
@@ -1,5 +1,4 @@
 // Toggle class active
-
 const navbarNav = document.querySelector(".navbar-nav");
 
 // Ketika hamberger-menu di click
@@ -8,11 +7,19 @@ document.querySelector("#hamburger-menu").onclick = () => {
 };
 
 // Click diluar side bar untuk menghilangkan navbar
-
 const hamburger = document.querySelector("#hamburger-menu");
 
 document.addEventListener("click", function (e) {
   if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
     navbarNav.classList.remove("active");
   }
+});
+
+// Close navbar on nav menu select
+const navItems = document.querySelectorAll(".navbar-nav a");
+
+navItems.forEach((nav) => {
+  nav.addEventListener("click", () => {
+    navbarNav.classList.remove("active");
+  });
 });
